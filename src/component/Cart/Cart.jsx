d472e0adb2cd4b7aff92547b0fa8d2670b15521e
fakrul-hossain <fakrulhossain912@gmial.com>
2023-03-28 15:38:51 +0600

    added cart compotent

---
+++ src/component/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = ({cart}) => {
+    // const {cart} = props
+    console.log(cart)
+
+    let total  = 0;
+    let totalShipping  = 0;
+    for (const product of cart) {
+        total = total + product.price
+        totalShipping  = totalShipping  + product.shipping  
+    }
+    const tax = total*7/100
+    const grandTotal =  total + totalShipping + tax;
+    return (
+        <div className='cart-summary'>
+        <h3>Order Summery</h3>
+        <p>Selected Items: {cart.length}</p>
+        <p>Total Price : ${total}</p>
+        <p>Total Shipping ${totalShipping}</p>
+        <p>Tax$ {tax.toFixed(2)}</p>
+        <h5>Grand Total $ {grandTotal.toFixed(2)}</h5>
+        </div>
+    );
+};
+
+export default Cart;
